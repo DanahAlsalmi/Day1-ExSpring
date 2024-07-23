65221d7c4b1f3bc04f8749ad860524830eb7b086
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-07-23 14:30:54 +0300

    Add files via upload

---
+++ src/main/java/com/example/day1exspring/Day1ExSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.day1exspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1ExSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1ExSpringApplication.class, args);
+    }
+
+}
